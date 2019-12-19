1b88d32911f9947c1c47a9d45981177691c996a0
Yacine SERIR <yserir@egencia.com>
2019-12-19 15:22:58 +0100

    factorizing

--- basic_crawler.py
+++ basic_crawler.py
@@ -19,25 +19,29 @@ def _steamdb_request(URL_BASE_TAG = "https://steamdb.info/tags/?tagid=492", file
         html_sample = open("sample.html", "w", encoding=r.encoding)
         html_sample.write(r.text)
 
+    return r.text
     # print(r.content)
 
-# _steamdb_request(file_save=True)
-html_sample = open("sample.html", "r", encoding="UTF-8")
-# print(html_sample.readlines())
-sample = ''.join(html_sample.readlines())
+def _steamdb_all_app_indie(file_save =  False):
+    if file_save:
+        html_sample = open("sample.html", "r", encoding="UTF-8")
+    else:
+        html_sample = _steamdb_request(file_save=True)
+        
+    sample = ''.join(html_sample.readlines())
 
-# print(type(sample))
+    soup = BeautifulSoup(sample, "lxml")
+    all_td = soup.find_all("td", class_="text-left")
+    apps = {}
+    for td in all_td:
+        # print(td.a)
+        if td.a and td.a.get('href'):
+            # print(td.a.get('href'))
+            apps[td.a.get('href').split('/')[-2]] = td.a.get_text()
 
-soup = BeautifulSoup(sample, "lxml")
-file = open("out.txt", "w", encoding="UTF-8")
-all_td = soup.find_all("td", class_="text-left")
-apps = {}
-for td in all_td:
-    # print(td.a)
-    if td.a and td.a.get('href'):
-        # print(td.a.get('href'))
-        apps[td.a.get('href').split('/')[-2]] = td.a.get_text()
-
-
-file.write(str(apps))
+    if file_save:
+        file = open("out.txt", "w", encoding="UTF-8")
+        file.write(str(apps))
+    
+    return apps
 
