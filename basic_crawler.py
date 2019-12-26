c7caad5d74d5afea05327546477573c600d0f308
Yacine SERIR <yserir@egencia.com>
2019-12-26 15:02:36 +0100

    cleaning

--- basic_crawler.py
+++ basic_crawler.py
@@ -18,14 +18,10 @@ def _emulate_agent():
     return USER_AGENT
 
 # Requesting The Page
-def _steamdb_request(URL_BASE_TAG="https://steamdb.info/tags/?tagid=492",file_save=False):
+def _steamdb_request(URL_BASE_TAG="https://steamdb.info/tags/?tagid=492"):
     r = requests.get(URL_BASE_TAG, headers=_emulate_agent())
     print(r.status_code)
     print(r.encoding)
-
-    if file_save :
-        html_sample = open("sample.html", "w", encoding=r.encoding)
-        html_sample.write(r.text)
 
     return r.text
     # print(r.content)
@@ -73,7 +69,7 @@ def _steamdb_all_app_indie(file_save =  False):
         jfile = open("apps_list.json", "r+")
         apps = json.load(jfile)
     else:
-        sample = _steamdb_request(file_save=file_save)
+        sample = _steamdb_request()
         soup = BeautifulSoup(sample, "lxml")
         all_td = soup.find_all("td", class_="text-left")
         apps = {}
